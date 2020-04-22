1c75351757010771df651cd8469e4970f0d995a3
AmmonMatthews <ammonmatthews@gmail.com>
2020-04-22 11:06:30 -0600

    pm2 initialized

--- addressbook/app.js
+++ addressbook/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var personsRouter = require('./routes/persons');
 
 var app = express();
 
@@ -15,6 +16,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/persons', personsRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
